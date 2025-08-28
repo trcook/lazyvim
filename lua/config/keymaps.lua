7e4082364550c413e8e6cbe175f77894e31e2795
Tom Cook <tom_cook@local>
2025-08-28 09:04:15 -0500

    bump

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,17 +2,26 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
-map("i", "kj", "<ESC>")
+map({ "i", "c" }, "kj", "<ESC>")
 map("n", ";", ":")
 map("i", "<M-f>", "<C-o>e")
 map("i", "<M-b>", "<C-o>b")
 
 map("i", "<C-k>", "<esc>ld$a")
-map("i", "<C-e>", "<end>")
-map("i", "<C-a>", "<home>")
+map({ "i", "n" }, "<C-e>", "<end>")
+map({ "i", "n" }, "<C-a>", "<home>")
 
 map("i", "<C-f>", "<right>")
 map("i", "<C-b>", "<left>")
 map("i", "<C-n>", "<down>")
 map("i", "<C-p>", "<up>")
 map("i", "<C-h>", "<BS>")
+map("n", "<Tab>", function()
+  vim.cmd("bnext")
+end)
+map("n", "<S-Tab>", function()
+  vim.cmd("bNext")
+end)
+
+map({ "i", "n" }, "<M-/>", vim.lsp.buf.signature_help, { desc = "get signature help" })
+map({ "i", "n" }, "<M-/>", vim.lsp.buf.hover)
--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -10,5 +10,18 @@ return {
       -- add a keymap
     end,
   },
-
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.lsp.signature = {
+        auto_open = { enabled = false },
+      }
+    end,
+  },
+  {
+    "Saghen/blink.cmp",
+    opts = function(_, opts)
+      opts.completion.menu.auto_show = false
+    end,
+  },
 }
