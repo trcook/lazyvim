f2449700bb9953e5eb76b378f2311f06b5721bdb
Tom Cook <tom_cook@local>
2025-08-28 18:48:38 -0500

    bump

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@
 
 require("config.lazy")
 -- vim.o.background = "dark"
+
+-- modify flash label so it is more legible, regardless of the theme.
+vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#ffffff", bg = "#d20065" })
