23facee1b1ad346d64964007ed8c0a45f549b0d1
Thomas Cook <thomas.cook@kc.frb.org>
2025-09-02 14:07:44 -0500

    bump

--- lua/plugins/pylsp.lua
+++ lua/plugins/pylsp.lua
@@ -21,10 +21,10 @@ return {
           severity_sort = true,
           signs = {
             text = {
-              [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
-              [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
-              [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
-              [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+              -- [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+              -- [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+              -- [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+              -- [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
             },
           },
         },
