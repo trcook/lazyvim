3deabde7904af8edd20fdb407279325e0dc77c16
Tom Cook <thomas.cook@kc.frb.org>
2025-09-03 11:03:50 -0500

    bump

--- lua/plugins/pylsp.lua
+++ lua/plugins/pylsp.lua
@@ -13,7 +13,7 @@ return {
           virtual_text = {
             spacing = 4,
             source = "if_many",
-            prefix = "●",
+            prefix = " ",
             -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
             -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
             -- prefix = "icons",
@@ -21,10 +21,10 @@ return {
           severity_sort = true,
           signs = {
             text = {
-              -- [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
-              -- [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
-              -- [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
-              -- [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+              [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+              [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+              [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+              [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
             },
           },
         },
