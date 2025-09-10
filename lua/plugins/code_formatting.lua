f5f28d362a542fcf71c94d8e55246585b0b190b6
Tom Cook <thomas.cook@kc.frb.org>
2025-09-10 14:29:02 -0500

    bump

---
+++ lua/plugins/code_formatting.lua
@@ -0,0 +1,11 @@
+-- add options here to control how code is formatted/autoformatted
+return {
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      -- You need to set "vim.g.autoformat = false" in init.lua to permenantly disable autoformatting. it can still be run on with <leader>cf and it can still be turned back on with <leader>uf
+      format_on_save = false,
+    },
+  },
+}
