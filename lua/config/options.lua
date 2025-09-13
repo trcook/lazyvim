5212dbe62ec864de9c58121851186a0d3cff245b
Thomas Cook <thomas.cook@kc.frb.org>
2025-09-13 09:58:06 -0500

    bump

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.breakindentopt = "shift:2" -- Optional: Add an extra 2 spaces of indentation
+vim.opt.linebreak = true
+return {}
