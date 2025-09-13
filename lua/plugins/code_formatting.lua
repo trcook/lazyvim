ace24ad6688f6aaffcc585f62a3ae0192e394ccc
Tom Cook <tom_cook@local>
2025-09-13 13:40:21 -0500

    bump

--- lua/plugins/code_formatting.lua
+++ lua/plugins/code_formatting.lua
@@ -6,6 +6,9 @@ return {
     opts = {
       -- You need to set "vim.g.autoformat = false" in init.lua to permenantly disable autoformatting. it can still be run on with <leader>cf and it can still be turned back on with <leader>uf
       format_on_save = false,
+      formatters_by_ft = {
+        json = {  "prettier", stop_after_first = true },
+      },
     },
   },
 }
